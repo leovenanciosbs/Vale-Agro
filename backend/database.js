323d7bfc505260ce18b09483e9fbccb69949d66f
leovenanciosbs <leovenanciobr@gmail.com>
2026-07-20 21:15:27 -0300

    up 1.2

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const { AsyncLocalStorage } = require('async_hooks');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -16,9 +17,15 @@ const pool = new Pool({
   ssl: useSsl ? { rejectUnauthorized: false } : false
 });
 
+const transactionContext = new AsyncLocalStorage();
+
 function convertSql(sql) {
   let index = 1;
   return sql.replace(/\?/g, () => `$${index++}`);
+}
+
+function getCurrentClient() {
+  return transactionContext.getStore();
 }
 
 const db = {
@@ -27,8 +34,12 @@ const db = {
       callback = params;
       params = [];
     }
+
     const pgSql = convertSql(sql);
-    pool.query(pgSql, params, (err, res) => {
+    const client = getCurrentClient();
+    const executor = client || pool;
+
+    executor.query(pgSql, params, (err, res) => {
       if (err) {
         callback(err, null);
       } else {
@@ -36,14 +47,18 @@ const db = {
       }
     });
   },
-  
+
   all: (sql, params, callback) => {
     if (typeof params === 'function') {
       callback = params;
       params = [];
     }
+
     const pgSql = convertSql(sql);
-    pool.query(pgSql, params, (err, res) => {
+    const client = getCurrentClient();
+    const executor = client || pool;
+
+    executor.query(pgSql, params, (err, res) => {
       if (err) {
         callback(err, null);
       } else {
@@ -51,29 +66,92 @@ const db = {
       }
     });
   },
-  
+
   run: (sql, params, callback) => {
     if (typeof params === 'function') {
       callback = params;
       params = [];
     }
+
+    const sqlTrim = String(sql || '').trim();
+    const upperSql = sqlTrim.toUpperCase();
+    const client = getCurrentClient();
+
+    const handleResult = (err, res) => {
+      if (callback) {
+        if (err) callback.call({}, err);
+        else {
+          const isInsert = sqlTrim.toUpperCase().startsWith('INSERT');
+          const resultCtx = {
+            changes: res ? res.rowCount : 0,
+            lastID: isInsert && res && res.rows.length > 0 ? res.rows[0].id : undefined
+          };
+          callback.call(resultCtx, null);
+        }
+      }
+    };
+
+    if (upperSql === 'BEGIN TRANSACTION' || upperSql === 'BEGIN') {
+      if (client) {
+        const error = new Error('Transaction already in progress.');
+        if (callback) callback.call({}, error);
+        return Promise.resolve();
+      }
+
+      return pool.connect().then(transactionClient => {
+        return transactionClient.query('BEGIN').then(() => {
+          transactionContext.enterWith(transactionClient);
+          if (callback) callback.call({ changes: 0 }, null);
+        }).catch(err => {
+          transactionClient.release();
+          if (callback) callback.call({}, err);
+          throw err;
+        });
+      });
+    }
+
+    if (upperSql === 'COMMIT' || upperSql === 'ROLLBACK') {
+      if (!client) {
+        const error = new Error('No transaction in progress.');
+        if (callback) callback.call({}, error);
+        return Promise.resolve();
+      }
+
+      return client.query(convertSql(sql), params)
+        .then(res => {
+          client.release();
+          transactionContext.enterWith(null);
+          if (callback) callback.call({ changes: res.rowCount, lastID: undefined }, null);
+        })
+        .catch(err => {
+          client.release();
+          transactionContext.enterWith(null);
+          if (callback) callback.call({}, err);
+          throw err;
+        });
+    }
+
     const pgSql = convertSql(sql);
+    const executor = client || pool;
     const isInsert = pgSql.trim().toUpperCase().startsWith('INSERT');
     const finalSql = isInsert ? `${pgSql} RETURNING id` : pgSql;
 
-    pool.query(finalSql, params, (err, res) => {
-      if (err) {
-        if (callback) callback.call({}, err);
-      } else {
-        const resultCtx = {
+    if (callback) {
+      executor.query(finalSql, params, handleResult);
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      executor.query(finalSql, params, (err, res) => {
+        if (err) return reject(err);
+        resolve({
           changes: res.rowCount,
           lastID: isInsert && res.rows.length > 0 ? res.rows[0].id : undefined
-        };
-        if (callback) callback.call(resultCtx, null);
-      }
+        });
+      });
     });
   },
-  
+
   serialize: (cb) => {
     if (cb) cb();
   }
